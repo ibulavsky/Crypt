30124f82175b8ee48704c5096a5bb9d937d9c7fe
ibulavsky <ibulavsky@gmail.com>
2020-09-04 15:00:51 +0300

    Updates

---
+++ precache-manifest.192f68d8d58bc11dcb6c317dbd699254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802df477d3f2c006076f76026acf1009",
+    "url": "/crypt-react/index.html"
+  },
+  {
+    "revision": "e30682fa81aefc327a60",
+    "url": "/crypt-react/static/css/main.b8c4ab31.chunk.css"
+  },
+  {
+    "revision": "80be6c7afb611d4b4e64",
+    "url": "/crypt-react/static/js/2.51fa7739.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/crypt-react/static/js/2.51fa7739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30682fa81aefc327a60",
+    "url": "/crypt-react/static/js/main.277d3804.chunk.js"
+  },
+  {
+    "revision": "16db5887621620668752",
+    "url": "/crypt-react/static/js/runtime-main.0e48e68d.js"
+  }
+]);
